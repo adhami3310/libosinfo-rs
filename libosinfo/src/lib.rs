65129d92a82552e88c68d8f9862973a9771788eb
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2023-06-19 18:04:22 -0400

    init commit

---
+++ libosinfo/src/lib.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
+/// No-op.
+macro_rules! skip_assert_initialized {
+    () => {};
+}
+
+/// No-op.
+macro_rules! assert_initialized_main_thread {
+    () => {};
+}
+
+pub use auto::*;
+mod auto;
